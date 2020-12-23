84e6f53f6cbf379371792d8aaa1959ee94cdcf8f
Daniel Morrison <daniel.morrison@oramon.nl>
2020-12-23 14:45:20 +1100

    more code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,59 @@
 import * as _ from 'lodash'
 import { Tabletojson } from 'tabletojson'
 
-type Rate = {
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',]
+
+type TableRate = {
   [key: string]: string
 }
 
-Tabletojson.convertUrl(fy1920,
-  { useFirstRowForHeadings: true }
-)
-.then((tableData: Rate[][]) => {
-  const flat = _.flatten(tableData)
+type DateRate = {
+  date: Date,
+  rate: number,
+}
+
+const runner = async () => {
+
+  const urls = [
+    'https://www.ato.gov.au/Tax-professionals/TP/Monthly-exchange-rates-for-1-July-2019-to-30-June-2020/',
+    'https://www.ato.gov.au/Tax-professionals/TP/1-July-2018-to-30-June-2019/',
+    'https://www.ato.gov.au/Tax-professionals/TP/1-July-2017-to-30-June-2018/',
+    'https://www.ato.gov.au/Tax-professionals/TP/1-July-2016-to-30-June-2017/',
+  ]
+
+
+  const tables: TableRate[][] = await Promise.all(urls.map(url => Tabletojson.convertUrl(url, { useFirstRowForHeadings: true })))
+
+  const flat = _.flattenDeep(tables)
   const euro = _.filter(flat, (rate) => rate['Country'] === 'Europe' || rate['Country and currency'] === 'European euro')
-  const rates: Rate[] = euro.map(rate => _.pickBy(rate, (value, key) => key.includes('Average')))
-  console.log(Object.assign({}, ...rates))
+  const rates: TableRate[] = euro.map(rate => _.pickBy(rate, (_value, key) => key.includes('Average')))
+  const r: DateRate[] = _.flatten(rates.map(obj => Object.entries(obj).map(
+    ([key, value]) => {
+      const repl = encodeURI(key).replace(/%C2%A0/g, ' ')
+      const [month, year] = decodeURI(repl).split(' ')
+
+      const date = new Date()
+      date.setFullYear(Number.parseInt(year), months.indexOf(month), 1)
+      date.setHours(0, 1, 0, 0)
+      const rate = Number.parseFloat(value) || 0.0
+
+      return { date, rate }
+    }
+  )))
+
+  return r.sort((a, b) => {
+    return b.date.getTime() - a.date.getTime()
+  })
+}
+
+runner()
+.then(rates => {
+  const best = _.maxBy(rates, 'rate')
+  if (!best) {
+    console.error('Could not find a valid rate')
+  } else {
+    console.log(`best rate is ${best.rate} on ${best.date}`)
+  }
 })
+.catch(err => console.error(err))
+
