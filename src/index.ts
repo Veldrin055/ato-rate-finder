c5b2dc7d6fe6da999d142b2030fa0f3b77b2683c
Daniel Morrison <daniel.morrison@oramon.nl>
2020-12-28 11:24:24 +1100

    ansi colours!

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import prompts from 'prompts'
+import c from 'ansi-colors'
 
 import { arrayWindow } from './arrayWindow'
 import { getRates } from './ato'
@@ -15,23 +16,36 @@ const selectDate = async (dates: DateRate[]) => {
   return response.month
 }
 
+const continuePrompt = async () => {
+  const toggle = await prompts({
+    type: 'toggle',
+    name: 'value',
+    initial: false,
+    message: 'Do another one?',
+    active: 'Yes',
+    inactive: 'No',
+  })
+
+  return toggle.value
+}
+
 const findOptimalAverage = async (rates: DateRate[]) => {
   const pivot = await selectDate(rates)
 
   let best = 0
   let msg = ''
-  console.log(`selected date is ${rates[pivot].date}`)
+  console.log(`Selected date is ${rates[pivot].date}`)
   arrayWindow(rates, pivot, 12, (window) => {
     // console.log(`run ${i++}=${JSON.stringify(window)}, size=${window.length}`)
     const average = window.reduce((a, b) => (a + b.rate), 0) / window.length
     // console.log(`run=${i++}, avg=${average}, from ${window[0].date} to ${window[window.length - 1].date}`)
     if (average > best) {
       best = average
-      msg = `best rate is ${best}, from ${formatDate(window[0].date)} to ${formatDate(window[window.length - 1].date)}`
+      msg = `Best rate is ${c.bold.green(best.toFixed(4))}, from ${c.bold.yellow(formatDate(window[0].date))} to ${c.bold.magenta(formatDate(window[window.length - 1].date))}`
     }
   })
   if (!best) {
-    console.error('Could not find a valid rate')
+    console.error(c.red('Could not find a valid rate'))
   } else {
     console.log(msg)
   }
@@ -40,19 +54,11 @@ const findOptimalAverage = async (rates: DateRate[]) => {
 (async () => {
   console.log('Downloading rates from ATO...')
   const rates = await getRates()
-  console.log('Done')
+  console.log(c.green('Done'))
   let cont = true
   while (cont) {
     await findOptimalAverage(rates)
-    const toggle = await prompts({
-      type: 'toggle',
-      name: 'value',
-      initial: false,
-      message: 'Do another one?',
-      active: 'Yes',
-      inactive: 'No',
-    })
-    cont = toggle.value
+    cont = await continuePrompt()
   }
   
 })()
